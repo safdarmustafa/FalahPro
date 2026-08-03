7a81025d27a9b5ef1cb1452ef15c90ac73d9c591
safdarmustafa <razasaif276@gmail.com>
2026-08-04 00:02:00 +0530

    version code 3 pushing to play store in closed testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.falahpro.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
