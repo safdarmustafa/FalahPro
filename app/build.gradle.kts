9bec34c077b9100beee0b60decbd6acd6fe46e92
safdarmustafa <razasaif276@gmail.com>
2026-08-04 17:39:25 +0530

    version 4 again fixed the google sign issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.falahpro.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
